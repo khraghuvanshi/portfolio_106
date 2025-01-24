fe4a440f6ebb56ae68fa24fef04cec827352d277
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-01-24 13:18:19 -0800

    JS home

--- global.js
+++ global.js
@@ -26,11 +26,12 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-// const ARE_WE_HOME = document.documentElement.classList.contains('Home');
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 
