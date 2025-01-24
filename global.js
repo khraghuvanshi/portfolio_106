c1f3436cf0d3e91b8a9818f9df38336b0c46683b
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-01-24 12:57:59 -0800

    Update global.js

--- global.js
+++ global.js
@@ -33,11 +33,17 @@ for (let p of pages) {
   let title = p.title;
 
   url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
 
-  let a = document.createElement('a');
-  a.href = url;
-  a.textContent = title;
-  nav.append(a);
+
+  // let a = document.createElement('a');
+  // a.href = url;
+  // a.textContent = title;
+  // nav.append(a);
 
 //   nav.insertAdjacentHTML('beforeend', `<a href="${url}" target="${url.startsWith('http') ? '_blank' : '_self'}">${title}</a>`);
 }
