4c345649ef34ec54c1706f4a0e1372d81f8081f9
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-01-24 13:50:03 -0800

    Update global.js

--- global.js
+++ global.js
@@ -39,8 +39,11 @@ for (let p of pages) {
   if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
   }
-  a.toggleAttribute('target', a.host !== location.host);
   
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
   nav.append(a);
 
 }
