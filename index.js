f4bebcad83c77cfa14db082b225c527e53437cdf
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-01-31 14:17:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
-const projects = await fetchJSON('./lib/projects.json');
+import { fetchJSON, renderProjects, fetchGithubData } from 'global.js';
+const projects = await fetchJSON('projects/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
