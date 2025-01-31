713462266efc2241943daf813e22be2a43f5363f
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-01-31 14:04:19 -0800

    project count

--- projects/projects.js
+++ projects/projects.js
@@ -3,9 +3,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
-
-// const projectsTitle = document.querySelector('.projects-title');
+const projectsTitle = document.querySelector('.projects-title');
 
 renderProjects(projects, projectsContainer, 'h2');
+projectsTitle.innerHTML = `Here are my <span>${projects.length}</span> projects`;
 
 // projectsTitle.textContent = `Here are my ${projects.length} projects`;
