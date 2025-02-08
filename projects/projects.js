9ac37b5ccd6baf8cbf4cf4b252208f067ef83721
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-08 14:43:43 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -20,3 +20,30 @@ let arc = arcGenerator({
   });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2];
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors[idx % colors.length]) // Fill in the attribute for fill color via indexing the colors variable
+})
