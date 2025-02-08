cf97e4ac358ab2ab90e46a9b34f91a23003f6244
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-08 14:33:43 -0800

    SVG

--- projects/projects.js
+++ projects/projects.js
@@ -9,3 +9,14 @@ renderProjects(projects, projectsContainer, 'h2');
 projectsTitle.innerHTML = `Here are my <span>${projects.length}</span> projects`;
 
 // projectsTitle.textContent = `Here are my ${projects.length} projects`;
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
