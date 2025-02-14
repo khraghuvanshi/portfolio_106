960c3e3c064a6e3f4bf21e35d1b45277332a0a31
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-14 15:35:04 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -11,6 +11,38 @@ async function loadData() {
       }));
 }
 
+function processCommits() {
+    commits = d3
+      .groups(data, (d) => d.commit) // Group data by commit
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+  
+        // Construct the commit object with basic and derived information
+        let ret = {
+          id: commit,
+          url: 'https://github.com/YOUR_REPO/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,  // Calculate fractional hour
+          totalLines: lines.length,  // Number of lines modified in this commit
+        };
+  
+        // Optionally hide the lines array
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          writable: false, // Prevent changes
+          enumerable: false, // Hide in normal printing
+          configurable: false, // Can't redefine or delete this property
+        });
+  
+        return ret;
+      });
+    }
+
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
