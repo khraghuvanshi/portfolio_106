5866e24f582c6c358c7a34865ab54f23632176e1
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-14 21:05:29 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -116,6 +116,14 @@ function createScatterplot() {
     // Y Scale: Linear Scale for Hour Fraction
     const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
 
+    const gridlines = svg
+  .append('g')
+  .attr('class', 'gridlines')
+  .attr('transform', `translate(${usableArea.left}, 0)`);
+
+// Create gridlines as an axis with no labels and full-width ticks
+gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
     // Draw Dots
     const dots = svg.append('g').attr('class', 'dots');
 
